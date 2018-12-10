09fedebf663e4daaad4d3439c9cc2ca8c840b98c
Baderf <badf179@gmail.com>
2018-12-10 15:13:10 +0200

    Initial Commit

---
+++ BuilidingEscape/Source/BuilidingEscape/BuilidingEscapeGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BuilidingEscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILIDINGESCAPE_API ABuilidingEscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
