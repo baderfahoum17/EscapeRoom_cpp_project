6ac05f2a869933ede01995ac5b4eba3c68f76ed1
Baderf <badf179@gmail.com>
2018-12-11 13:57:27 +0200

    BulidingEscape PreAccessingObjectNames commit

---
+++ BuilidingEscape/Source/BuilidingEscape/SofaPositionReport.cpp
@@ -0,0 +1,33 @@
+// copy right bader fahoum2018
+
+#include "SofaPositionReport.h"
+
+// Sets default values for this component's properties
+USofaPositionReport::USofaPositionReport()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void USofaPositionReport::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void USofaPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
